3d12ff2fb226ace49756a496bd75b196fa077325
Gustavo Rezende <nsigustavo@gmail.com>
2009-07-06 03:18:00 -0300

    modificado long_description de setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
 import sys, os
-
+import re
 version = '0.0'
+
+
+
+def read(*rnames):
+    return file(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+text = (
+    read('ludibrio/__init__.py')
+    )
+init = text.find('"""')
+fim = text.find('"""', init+1)
+long_description = text[init:fim]
+
 
 setup(name='ludibrio',
       version=version,
       description="Platform for test doubles in Python (mocks, stubs, fakes, and dummies)",
-      long_description="""\
-""",
+      long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='mock stub fake dummy dobles mocks',
       author='nsigustavo@gmail.com',
