b5fff558f9db1fb90ef2f929a234a9ad03a17fce
Hugo Lopes Tavares <hltbra@gmail.com>
2009-08-26 20:52:43 -0300

    agora está compativel com python2.5 e python2.6 ;-)

--- ludibrio/_testludibrio.py
+++ ludibrio/_testludibrio.py
@@ -1,3 +1,6 @@
+from __future__ import with_statement
 import doctest
-print doctest.testfile("../README", optionflags=doctest.REPORT_ONLY_FIRST_FAILURE + doctest.ELLIPSIS)
+print doctest.testfile("../README",
+                       optionflags=doctest.REPORT_ONLY_FIRST_FAILURE + doctest.ELLIPSIS,
+                       globs=globals())
 
