ca46c2b955d09ac1db7f4e6e96bcffeea0c41708
Gustavo Rezende <nsigustavo@gmail.com>
2009-09-21 02:15:48 -0300

    Corrigido bugs de chamadas de sistames __.*__ e corrigido msg de error para chamadas diferente no mock

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1'
+version = '2.0'
 
 
 
@@ -34,3 +34,4 @@ setup(name='ludibrio',
       # -*- Entry points: -*-
       """,
       )
+
