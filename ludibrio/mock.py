95fda9b34a38a3f4e2f96215895cfcf2ea62529c
gustavo <gustavo@gustavo-laptop.(none)>
2010-05-11 17:09:39 -0300

    aplicados paths de Rodrigo Manhas

--- ludibrio/mock.py
+++ ludibrio/mock.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 
-#TODO: mockar imports respeitando o escopo com frame, pode-se subistituir o importe e validar a achamada
+#TODO: mockar imports respeitando o escopo com frame, pode-se subistituir o importe e validar a acall
 #TODO: Spy ainda esta como protótipo
 
 from __future__ import with_statement 
@@ -16,13 +16,12 @@ STOPRECORD = False
 RECORDING = True 
 
 
-
 class Mock(_TestDouble):
     """Mocks are what we are talking about here:
     objects pre-programmed with expectations which form a
     specification of the calls they are expected to receive.
     """
-    __expectation__ =[]#[ChamadaMockda(attribute, args, kargs),]
+    __expectation__ =[]#[MockedCall(attribute, args, kargs),]
     __recording__ = RECORDING 
     __lastPropertyCalled__ = None 
 
@@ -33,14 +32,14 @@ class Mock(_TestDouble):
         return self 
 
     def __methodCalled__(self, *args, **kargs):
-        property = self.__lastPropertyCalled__ or getframeinfo(getframe(1))[2]# nome da funcao chamada
+        property = self.__lastPropertyCalled__ or getframeinfo(getframe(1))[2]# nome da funcao call
         self.__lastPropertyCalled__ = None 
         # property ==  __call__ or alias
         return self.__propertyCalled(property, args, kargs)
 
     def __propertyCalled(self, property, args=[], kargs={}):
         if self.__recording__:
-            self.__newExpectation(ChamadaMockda(property, args = args, kargs = kargs, retorno = self))
+            self.__newExpectation(MockedCall(property, args = args, kargs = kargs, response = self))
             return self 
         else:
             return self.__espectativaMockada(property, args, kargs)
@@ -57,15 +56,15 @@ class Mock(_TestDouble):
     def __newExpectation(self, attr):
         self.__expectation__.append(attr)
 
-    def __rshift__(self, retorno):
-            self.__expectation__[-1].setretorno(retorno)
+    def __rshift__(self, response):
+            self.__expectation__[-1].setResponse(response)
     __lshift__ = __rshift__ 
 
     def __espectativaMockada(self, attr, args=[], kargs={}):
         if not self.__expectation__:
-            raise AssertionError("Object's mocks are not pre-programmed with expectations")
-        chamadaMockada = self.__expectation__.pop(0)
-        return chamadaMockada.chamada(attr, args, kargs)
+            raise MockExpectationError("Mock Object received unexpected call: %s" % self.__lastPropertyCalled__)
+        callMockada = self.__expectation__.pop(0)
+        return callMockada.call(attr, args, kargs)
 
     def __getattr__(self, x):
         self.__lastPropertyCalled__ = x 
@@ -73,47 +72,51 @@ class Mock(_TestDouble):
 
     def validate(self):
         if self.__expectation__:
-            raise AssertionError, "Object's mocks are not pre-programmed with expectations"
+            raise MockExpectationError("Mock Object expected %s(), but didn't "
+                                 "received it" % self.__expectation__[0].args[0])
 
     def __del__(self):
         if self.__expectation__:
             print "Object's mocks are not pre-programmed with expectations"
 
 
-class ChamadaMockda(object):
-    def __init__(self, atributo, args=[], kargs={}, retorno = None):
-        self.atributo = atributo 
+class MockedCall(object):
+    def __init__(self, attribute, args=[], kargs={}, response = None):
+        self.attribute = attribute 
         self.args = args 
         self.kargs = kargs 
-        self.retorno = retorno 
+        self.response = response 
 
     def __repr__(self):
-        return str((self.atributo, self.args, self.kargs))
+        return str((self.attribute, self.args, self.kargs))
 
-    def setretorno(self, retorno):
-        self.retorno = retorno 
+    def setResponse(self, response):
+        self.response = response 
 
-    def chamada(self, atributo, args=[], kargs={}):
-        if(self.atributo == atributo 
+    def call(self, attribute, args=[], kargs={}):
+        if(self.attribute == attribute 
         and self.args == args 
         and self.kargs == kargs):
-            if isinstance(self.retorno, Exception):
-                raise self.retorno 
+            if isinstance(self.response, Exception):
+                raise self.response 
             else:
-                return self.retorno 
+                return self.response 
         else:
-            self._erro(atributo, args, kargs)
+            self._erro(attribute, args, kargs)
 
-    def _erro(self, atributo, args, kargs):
-        raise AssertionError, "Object's mocks are not pre-programmed with expectations.\nGot:\n%s\nExpected:\n%s"%(
-            self._representacaoChamada(atributo, args, kargs),
-            self._representacaoChamada(self.atributo, self.args, self.kargs)
+    def _erro(self, attribute, args, kargs):
+        raise MockExpectationError, "Mock Object received unexpected call.\nExpected:\n%s\nGot:\n%s"%(
+            self._callRepresentation(self.attribute, self.args, self.kargs),
+            self._callRepresentation(attribute, args, kargs),
             )
 
-    def _representacaoChamada(self, attribute, args, kargs):
+    def _callRepresentation(self, attribute, args, kargs):
         if attribute == '__getattribute__':
             return args[0]
         return "%s(%s)"%(
                 attribute, 
                 ", ".join(["%r"%arg for arg in args]+["%s=%r"%(k, v)for k, v in kargs.items()]))
 
+
+class MockExpectationError(AssertionError):
+    '''Extends AssertionError for unittest compatibility'''
