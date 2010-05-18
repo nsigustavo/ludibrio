58faa2d25af5da245900ae65a565dd4035e56320
Hugo Lopes Tavares <hltbra@gmail.com>
2010-05-18 18:04:39 -0300

    merge conflicts resolved. back to last year branch

--- ludibrio/_testludibrio.py
+++ ludibrio/_testludibrio.py
@@ -1,6 +1,17 @@
 from __future__ import with_statement
 import doctest
-print doctest.testfile("../README",
-                       optionflags=doctest.REPORT_ONLY_FIRST_FAILURE + doctest.ELLIPSIS,
-                       globs=globals())
+import ludibrio
+import __future__
+import doctest
 
+
+#adiciona flags ao doctest para __future__ with_statement
+_extract_future_flags_old = doctest._extract_future_flags
+def _extract_future_flags(globs):
+        flags = _extract_future_flags_old(globs)
+        flags = flags +__future__.with_statement.compiler_flag
+        return flags
+doctest._extract_future_flags = _extract_future_flags
+
+
+print doctest.testfile("../README", optionflags=doctest.REPORT_ONLY_FIRST_FAILURE + doctest.ELLIPSIS  +__future__.with_statement.compiler_flag)
