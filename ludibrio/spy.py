88a12677dd7a5a45ba04e29e78e19f486f23acaa
BecaMotta <becacmotta@gmail.com>
2010-08-16 16:36:28 -0300

    Replacing true and false for expectations on spy

--- ludibrio/spy.py
+++ ludibrio/spy.py
@@ -1,4 +1,5 @@
 from ludibrio import Stub
+from ludibrio.helpers import format_called
 
 
 class Spy(Stub):
@@ -42,10 +43,18 @@ class verify(object):
         return self  
 
     def called(self, times):
-        return times.verify(
-                self.spy.called_count([self._attr_called,
-                                       self.args,
-                                       self.kargs]))
+        called_count = self.spy.called_count([self._attr_called,
+                                              self.args,
+                                              self.kargs])
+        if not times.verify(called_count):
+            raise SpyExpectationError("Spy expected %s %s%d times but received %d" % (
+                                        format_called(self._attr_called,
+                                                      self.args,
+                                                      self.kargs), 
+                                        times.operator_message,
+                                        times.expectation_value, 
+                                        called_count))
+    
     @property
     def before(self):        
         return Before(self)
@@ -56,29 +65,34 @@ class verify(object):
 
 
 class Times(object):
-
+    
     def _handle(self, operation, expectation_value):
         self.operation = operation
         self.expectation_value = expectation_value
         return self
 
     def __eq__(self, expectation_value):
+        self.operator_message = ''
         return self._handle(lambda x, y: x == y,
                              expectation_value)        
 
     def __gt__(self, expectation_value):
+        self.operator_message = 'more than '
         return self._handle(lambda x, y: x > y,
                              expectation_value)       
         
     def __ge__(self, expectation_value):
+        self.operator_message = 'more than or equal to '
         return self._handle(lambda x, y: x >= y,
                              expectation_value)
     
     def __lt__(self, expectation_value):
+        self.operator_message = 'less than '
         return self._handle(lambda x, y: x < y,
                              expectation_value)
         
     def __le__(self, expectation_value):
+        self.operator_message = 'less than or equal to '
         return self._handle(lambda x, y: x <= y,
                              expectation_value)
 
@@ -110,15 +124,16 @@ class TimeCalled(object):
 
 class Before(TimeCalled):
     def compare(self):
-        try:
-            return self.calls.index(self.before) < self.calls.index(self.after)
-        except ValueError:
-            return False
-
+        if not self.calls.index(self.before) < self.calls.index(self.after):
+            raise SpyExpectationError("Spy expected %s called before %s" %
+                                           (format_called(*self.before), format_called(*self.after)))
 
 class After(TimeCalled):
     def compare(self):
-        try:
-            return self.calls.index(self.before) > self.calls.index(self.after)
-        except ValueError:
-            return False         
+        if not self.calls.index(self.before) > self.calls.index(self.after):
+            raise SpyExpectationError("Spy expected %s called after %s" %
+                                           (format_called(*self.before), format_called(*self.after)))
+
+
+class SpyExpectationError(AssertionError):
+    """Extends AssertionError for unittest compatibility"""
